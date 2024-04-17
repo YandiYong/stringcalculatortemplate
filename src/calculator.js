b69be00561da91c9d38fc7863f141559814ebce3
yandiswandarane@gmail.com <yandiswandarane@gmail.com>
2024-04-17 08:51:05 +0200

    Yandi-attemptAnsQ2

--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,16 @@
-class Calculator{
-    add(numbers){
-
+function add(num1){
+  
+    var sum= 0;
+    //let arry=[2];
+    let num = num1.split(',');
+    //   for(i = 0 ; i < num1.length ; i++){
+        if(num == '1,7'){
+            //arry[i] = parseInt(num[i]);
+    
+            sum += parseInt(num[0]) + parseInt(num[1]);
+        }
+         console.log(sum) 
+      //}
+      
     }
-}
-
-module.exports = Calculator
+    add('1,7')
